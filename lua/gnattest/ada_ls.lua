b7feece62f2d3c24cb4c3b6a3c0146982f2cffca
Steven Bias <bias@adacore.com>
2025-11-04 20:39:01 +0100

    style: Format project
    style: Format project

--- lua/gnattest/ada_ls.lua
+++ lua/gnattest/ada_ls.lua
@@ -3,20 +3,20 @@ local utils = require("gnattest.utils")
 local M = {}
 
 function M.setup()
-	vim.api.nvim_create_autocmd("LspAttach", {
-		callback = function(ev)
-			local _, j = string.find(utils.get_bufdir(), "gnattest")
-			local gnattest_dir = string.sub(utils.get_bufdir(), 1, j)
-			local client = vim.lsp.get_client_by_id(ev.data.client_id)
-			if client.name == "ada" then
-				local ada_ls = assert(vim.lsp.get_clients({ name = "ada" })[1])
-				local config = {}
-				config["projectFile"] = gnattest_dir .. "/harness/test_driver.gpr"
-				config = { ada = config }
-				ada_ls.notify("workspace/didChangeConfiguration", { settings = config })
-			end
-		end,
-	})
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+      local _, j = string.find(utils.get_bufdir(), "gnattest")
+      local gnattest_dir = string.sub(utils.get_bufdir(), 1, j)
+      local client = vim.lsp.get_client_by_id(ev.data.client_id)
+      if client.name == "ada" then
+        local ada_ls = assert(vim.lsp.get_clients({ name = "ada" })[1])
+        local config = {}
+        config["projectFile"] = gnattest_dir .. "/harness/test_driver.gpr"
+        config = { ada = config }
+        ada_ls.notify("workspace/didChangeConfiguration", { settings = config })
+      end
+    end,
+  })
 end
 
 return M
--- lua/gnattest/utils.lua
+++ lua/gnattest/utils.lua
@@ -3,28 +3,28 @@ local utils = {}
 utils.plugin_name = "GNATtest"
 
 function utils.notify(msg, lvl)
-	local title = utils.plugin_name .. " " .. lvl .. " message"
-	if utils.is_loaded("notify") then
-		require("notify")(msg, lvl, { title = title })
-	else
-		vim.api.nvim_err_writeln(title .. ": " .. msg)
-	end
+  local title = utils.plugin_name .. " " .. lvl .. " message"
+  if utils.is_loaded("notify") then
+    require("notify")(msg, lvl, { title = title })
+  else
+    vim.api.nvim_err_writeln(title .. ": " .. msg)
+  end
 end
 
 function utils.is_loaded(plugin_name)
-	return pcall(require, plugin_name) -- will also load the package if it isn't loaded already
+  return pcall(require, plugin_name) -- will also load the package if it isn't loaded already
 end
 
 function utils.get_bufid()
-	return vim.api.nvim_get_current_buf()
+  return vim.api.nvim_get_current_buf()
 end
 
 function utils.get_bufdir()
-	return vim.fn.expand("%")
+  return vim.fn.expand("%")
 end
 
 function utils.is_gnattest_file()
-	return string.find(utils.get_bufdir(), "gnattest") ~= nil
+  return string.find(utils.get_bufdir(), "gnattest") ~= nil
 end
 
 return utils
