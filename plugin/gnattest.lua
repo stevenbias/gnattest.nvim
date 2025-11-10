6076a1721fb3998136b2eb23a6372516ffc60074
Steven Bias <bias_steven@hotmail.fr>
2025-11-10 23:09:27 +0100

    feat(commands): Create GNATtest command with subcommands

--- plugin/gnattest.lua
+++ plugin/gnattest.lua
@@ -1,9 +1,89 @@
+-- Define commands with subcommands, from: https://github.com/lumen-oss/nvim-best-practices?tab=readme-ov-file#speaking_head-user-commands
+
+local cmd_name = "GNATtest"
 local test_project = "obj/gnattest/harness/test_driver.gpr"
+local test_runner = "obj/gnattest/harness/test_runner"
 
-vim.api.nvim_create_user_command("GNATtestBuild", function()
-  vim.fn.system("gprbuild -P " .. test_project)
-end, {})
-
-vim.api.nvim_create_user_command("GNATtestClean", function()
+local function clean_tests()
   vim.fn.system("gprclean -P " .. test_project)
-end, {})
+end
+
+local function build_tests()
+  vim.fn.system("gprbuild -P " .. test_project)
+end
+
+local function run_tests()
+  vim.fn.system("./" .. test_runner)
+end
+
+---@class MyCmdSubcommand
+---@field impl fun(args:string[], opts: table) The command implementation
+---@field complete? fun(subcmd_arg_lead: string): string[] (optional) Command completions callback, taking the lead of the subcommand's arguments
+
+---@type table<string, MyCmdSubcommand>
+local subcommand_tbl = {
+  Clean = {
+    impl = function()
+      clean_tests()
+    end,
+  },
+  Build = {
+    impl = function()
+      build_tests()
+    end,
+  },
+  Run = {
+    impl = function()
+      run_tests()
+    end,
+  },
+}
+
+---@param opts table :h lua-guide-commands-create
+local function subcmd(opts)
+  local fargs = opts.fargs
+  local subcommand_key = fargs[1]
+  -- Get the subcommand's arguments, if any
+  local args = #fargs > 1 and vim.list_slice(fargs, 2, #fargs) or {}
+  local subcommand = subcommand_tbl[subcommand_key]
+  if not subcommand then
+    vim.notify(
+      cmd_name .. ": Unknown command: " .. subcommand_key,
+      vim.log.levels.ERROR
+    )
+    return
+  end
+  -- Invoke the subcommand
+  subcommand.impl(args, opts)
+end
+
+vim.api.nvim_create_user_command(cmd_name, subcmd, {
+  nargs = "+",
+  desc = cmd_name .. "commands",
+  complete = function(arg_lead, cmdline, _)
+    -- Get the subcommand.
+    local subcmd_key, subcmd_arg_lead =
+      cmdline:match("^['<,'>]*" .. cmd_name .. "[!]*%s(%S+)%s(.*)$")
+    if
+      subcmd_key
+      and subcmd_arg_lead
+      and subcommand_tbl[subcmd_key]
+      and subcommand_tbl[subcmd_key].complete
+    then
+      -- The subcommand has completions. Return them.
+      return subcommand_tbl[subcmd_key].complete(subcmd_arg_lead)
+    end
+    -- Check if cmdline is a subcommand
+    if cmdline:match("^['<,'>]*" .. cmd_name .. "[!]*%s+%w*$") then
+      -- Filter subcommands that match
+      local subcommand_keys = vim.tbl_keys(subcommand_tbl)
+      return vim
+        .iter(subcommand_keys)
+        :filter(function(key)
+          return key:find(arg_lead) ~= nil
+        end)
+        :totable()
+    end
+  end,
+  bang = true,
+})
