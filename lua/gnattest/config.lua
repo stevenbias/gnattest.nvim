e5805b0b2c6da57f7e4188601a08529b14c63956
Steven Bias <bias_steven@hotmail.fr>
2026-01-04 00:01:05 +0100

    refactor: Improve config validation check

--- lua/gnattest/config.lua
+++ lua/gnattest/config.lua
@@ -11,12 +11,29 @@ local default_opts = {
   },
 }
 
+local valid_keys = {
+  "highlight",
+  "read_only",
+}
+
 local M = {
   opts = default_opts,
 }
 
 local function is_valid(opts)
+  if opts == nil then
+    return true
+  end
+
   local notify = require("gnattest.utils").notify
+
+  for key in pairs(opts) do
+    if not vim.tbl_contains(valid_keys, key) then
+      notify("Unknown config field: " .. key, vim.log.levels.ERROR)
+      return false
+    end
+  end
+
   if opts.read_only and opts.read_only.region_text then
     local rt = opts.read_only.region_text
     if type(rt.start) ~= "string" or type(rt.ending) ~= "string" then
